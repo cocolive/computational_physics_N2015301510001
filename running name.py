0a9f965ba4350bc5ed31a447f40500eae2e0b63a
cocolive <31894878+cocolive@users.noreply.github.com>
2017-09-21 08:15:47 -0500

    Add files via upload

---
+++ running name.py
@@ -0,0 +1,16 @@
+print('祝轲', end='\r')
+import time
+time.sleep(0.7)
+print(end='\r      祝轲')
+import time
+time.sleep(0.7)
+print(end='\r            祝轲')
+import time
+time.sleep(0.7)
+print(end='\r                  祝轲')
+import time
+time.sleep(0.7)
+print(end='\r                       祝轲')
+import time
+time.sleep(0.7)
+print(end='\r                            祝轲')
